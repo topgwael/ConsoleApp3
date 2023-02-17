edb2b8a83aa75901d3f5bf9c72d3e602a0b2a9a5
Pingvu <nhlpool27@gmail.com>
2023-02-17 15:00:11 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace metoder
+{
+
+    class program
+    {
+
+        static void Main(string[]args)
+        {
+            
+            Console.WriteLine("skriv in höjden");
+            int höjden =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("skriv in bredden");
+            int bredden =int.Parse(Console.ReadLine());
+
+            int area = Area(bredden, höjden);
+
+            Console.WriteLine("Area är" + area);
+            
+           
+        }
+
+        static int Area(int bredden, int höjden)
+        {
+            return bredden * höjden;
+        }
+    }
+}
